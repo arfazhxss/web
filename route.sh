80bb2da5f73a67182b246c11a06e12fe0dd55a89
Arfaz Hossain <arfazhussain@uvic.ca>
2023-05-28 20:46:24 -0700

    bash script updated Scheduled Commit by @arfazhxss on Sun 28 May 2023 20:46:24 PDT

--- route.sh
+++ route.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-# route git 3.8
+# route git 4.2
+
+echo "Commit Message:"
+read commit_message
 
 git add . && \
 git add -u && \
-git commit -m $'Routine Commit\nby @arfazhxss on '"$(date)" && \
+git commit -m $"$commit_message"$'\nScheduled Commit by @arfazhxss on '"$(date)" && \
 git push origin HEAD
+git log > commit-hist.txt
+rm -rf .DS_Store/
