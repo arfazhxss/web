18f0975ce2c11fc6fcf53066e85721d801dc6c67
arfazhxss <arfazhussain@uvic.ca>
2023-07-02 18:03:43 -0700

    Bash Script Update V5 Commit by @arfazhxss on Sun 02 Jul 2023

--- route.sh
+++ route.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
-#auto-git v4.5
+#auto-git v6.0
 
-echo "Commit Message:"
-read commit_message
+echo -e "\n_______________________________________________________\n\n\t\tDELETE LOCAL CHANGES? (YES) \n\t\t\tOR\n\t\tPUSH LOCAL CHANGES (ENTER)\n"
+read -s -n 3 -p "(yes/ENTER): " answer
 
-if [[ -z "$commit_message" || ${#commit_message} -lt 3 ]]; then
-  commit_message="Routine Commit"
-  git add . && \
-  git add -u && \
-  git commit -m $"$commit_message"$'\nby @arfazhxss on '"$(date +'%a %d %b %Y')" && \
-  git push origin HEAD
-  git log > commit-hist.txt
-  rm -rf .DS_Store/
+if [[ $answer == "yes" || $answer == "Yes" || $answer == "YES" ]]; then
+  echo -e "YES'ED\n-------------------------------------------------------"
+  git stash
+  git stash clear
+  git pull
+  echo -e "-------------------------------------------------------\n\t\tYour Repository is synced\n\t\twith the latest commit :)\n_______________________________________________________"
 else
-  git add . && \
-  git add -u && \
-  git commit -m $"$commit_message"$'\nCommit by @arfazhxss on '"$(date +'%a %d %b %Y')" && \
-  git push origin HEAD
-  git log > commit-hist.txt
-  rm -rf .DS_Store/
+  echo -e "ENTER'ED\n-------------------------------------------------------"
+  read -p "Your Commit Message: " commt
+
+  if [[ -z "$commt" || ${#commt} -lt 3 ]]; then
+    commt="Routine Commit"
+    git add . && \
+    git add -u && \
+    git commit -m $"$commt"$'\nby @arfazhxss on '"$(date +'%a %d %b %Y')" && \
+    git push origin HEAD
+    git log > commit-hist.txt
+    rm -rf .DS_Store/
+    echo -e "-------------------------------------------------------\n\t\tYour changes has been pushed\n\t\tto the repository :)\n_______________________________________________________"
+  else
+    git add . && \
+    git add -u && \
+    git commit -m $"$commt"$'\nCommit by @arfazhxss on '"$(date +'%a %d %b %Y')" && \
+    git push origin HEAD
+    git log > commit-hist.txt
+    rm -rf .DS_Store/
+    echo -e "-------------------------------------------------------\n\t\tYour changes has been pushed\n\t\tto the repository :)\n_______________________________________________________"
+  fi
 fi
