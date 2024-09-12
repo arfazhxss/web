0bf93e1da6321aecc15c0dd84a1116bf7cfedbd9
Arfaz Hussain <arfazhussain.zn@gmail.com>
2024-09-11 17:46:01 -0700

    Routine Commit Commit by @arfazhxss on Wed 11 Sep 2024

--- design-4/src/data/resume.tsx
+++ design-4/src/data/resume.tsx
@@ -2,13 +2,13 @@ import { Icons } from "@/components/icons";
 import { HomeIcon, NotebookIcon } from "lucide-react";
 
 export const DATA = {
-  name: "Million Verma",
-  initials: "DV",
-  url: "https://dillion.io",
-  location: "San Francisco, CA",
+  name: "Arfaz Hussain",
+  initials: "AH",
+  url: "https://arfazhxss.ca",
+  location: "Victoria, British Columbia",
   locationLink: "https://www.google.com/maps/place/sanfrancisco",
   description:
-    "Software Engineer turned Entrepreneur. I love building things and helping people. Very active on Twitter.",
+    "Aspiring Software Engineer and full-stack developer",
   summary:
     "At the end of 2022, I quit my job as a software engineer to go fulltime into building and scaling my own SaaS businesses. In the past, [I pursued a double degree in computer science and business](/#education), [interned at big tech companies in Silicon Valley](https://www.youtube.com/watch?v=d-LJ2e5qKdE), and [competed in over 21 hackathons for fun](/#hackathons). I also had the pleasure of being a part of the first ever in-person cohort of buildspace called [buildspace sf1](https://buildspace.so/sf1).",
   avatarUrl: "/me.png",
