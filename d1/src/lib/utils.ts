329baac515110342c2f4b50c983576f9dc5dd1b1
Arfaz Hussain <arfazhussain.zn@gmail.com>
2024-09-14 21:37:04 -0700

    Routine Commit Commit by @arfazhxss on Sat 14 Sep 2024

--- d1/src/lib/utils.ts
+++ d1/src/lib/utils.ts
@@ -6,32 +6,56 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function formatDate(date: string) {
-  let currentDate = new Date().getTime();
+  let currentDate = new Date();
   if (!date.includes("T")) {
     date = `${date}T00:00:00`;
   }
-  let targetDate = new Date(date).getTime();
-  let timeDifference = Math.abs(currentDate - targetDate);
-  let daysAgo = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+  let targetDate = new Date(date);
 
-  let fullDate = new Date(date).toLocaleString("en-us", {
+  let fullDate = targetDate.toLocaleString("en-us", {
     month: "long",
     day: "numeric",
     year: "numeric",
   });
 
+  let yearDiff = currentDate.getFullYear() - targetDate.getFullYear();
+  let monthDiff = currentDate.getMonth() - targetDate.getMonth();
+  let dayDiff = currentDate.getDate() - targetDate.getDate();
+
+  if (dayDiff < 0) {
+    monthDiff--;
+    dayDiff += new Date(currentDate.getFullYear(), currentDate.getMonth(), 0).getDate();
+  }
+  if (monthDiff < 0) {
+    yearDiff--;
+    monthDiff += 12;
+  }
+
+  let timeDifference = currentDate.getTime() - targetDate.getTime();
+  let daysAgo = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+
+  const pluralize = (num: number, singular: string) =>
+    `${num} ${singular}${num !== 1 ? 's' : ''}`;
+
   if (daysAgo < 1) {
     return "Today";
   } else if (daysAgo < 7) {
-    return `${fullDate} (${daysAgo} days ago)`;
-  } else if (daysAgo < 30) {
-    const weeksAgo = Math.floor(daysAgo / 7);
-    return `${fullDate} (${weeksAgo} weeks ago)`;
-  } else if (daysAgo < 365) {
-    const monthsAgo = Math.floor(daysAgo / 30);
-    return `${fullDate} (${monthsAgo} months ago)`;
+    return `${fullDate} (${pluralize(daysAgo, 'day')} ago)`;
+  } else if (yearDiff > 0) {
+    if (monthDiff > 0) {
+      return `${fullDate} (${pluralize(yearDiff, 'year')} ${pluralize(monthDiff, 'month')} ago)`;
+    } else {
+      return `${fullDate} (${pluralize(yearDiff, 'year')} ago)`;
+    }
+  } else if (monthDiff > 0) {
+    if (daysAgo >= 14) {
+      const weeksAgo = Math.floor(daysAgo / 7);
+      return `${fullDate} (${pluralize(monthDiff, 'month')} ${pluralize(weeksAgo, 'week')} ago)`;
+    } else {
+      return `${fullDate} (${pluralize(monthDiff, 'month')} ago)`;
+    }
   } else {
-    const yearsAgo = Math.floor(daysAgo / 365);
-    return `${fullDate} (${yearsAgo} years ago)`;
+    const weeksAgo = Math.floor(daysAgo / 7);
+    return `${fullDate} (${pluralize(weeksAgo, 'week')} ago)`;
   }
 }
