a85235fb1c1e73e87635ddff83ac584476d49c9d
Arfaz Hussain <arfazhussain.zn@gmail.com>
2024-09-14 23:21:21 -0700

    Routine Commit Commit by @arfazhxss on Sat 14 Sep 2024

--- d1/src/app/blog/page.tsx
+++ d1/src/app/blog/page.tsx
@@ -15,7 +15,7 @@ export default async function BlogPage() {
   return (
     <section>
       <BlurFade delay={BLUR_FADE_DELAY}>
-        <h1 className="font-medium text-2xl mb-8 tracking-tighter">Arfaz's Blogs</h1>
+        <h1 className="font-medium text-2xl mb-8 tracking-tighter">Arfaz&apos;s Blogs</h1>
       </BlurFade>
       {posts
         .sort((a, b) => {
