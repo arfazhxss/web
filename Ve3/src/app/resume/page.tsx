6bdda0f290d0dec096dbff61817b244f7775fb36
Arfaz Hussain <arfazhussain.zn@gmail.com>
2024-10-14 09:44:19 -0700

    Routine Commit Commit by @arfazhxss on Mon 14 Oct 2024

--- Ve3/src/app/resume/page.tsx
+++ Ve3/src/app/resume/page.tsx
@@ -1,14 +1,10 @@
 "use client";
 import { Viewer, Worker } from '@react-pdf-viewer/core';
-import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
 import '@react-pdf-viewer/core/lib/styles/index.css';
-import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 
 export default function ResumePage() {
-    const defaultLayoutPluginInstance = defaultLayoutPlugin();
-
     return (
-        <div className="bg-background text-foreground max-w-7xl mx-auto px-4">
+        <div className="bg-background text-foreground max-w-7xl">
             <header className="mb-12 text-center">
                 <h1 className="font-bold mb-2 text-4xl md:text-4xl lg:text-5xl">
                     Arfaz Hossain
@@ -21,15 +17,13 @@ export default function ResumePage() {
                 <h2 className="font-semibold mb-3 text-lg md:text-lg lg:text-2xl">
                     My Resume
                 </h2>
-                <div className="w-full max-w-9xl mx-auto overflow-hidden rounded-lg border border-gray-600 bg-gray-800">
+                <div className="w-full max-w-9xl mx-auto overflow-hidden border bg-muted border-white-100">
                     <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
-                        <div style={{ height: 'calc(100vh - 200px)' }}>
-                            <Viewer
-                                fileUrl="/resume.pdf"
-                                plugins={[defaultLayoutPluginInstance]}
-                                theme="dark"
-                            />
-                        </div>
+                        <Viewer
+                            fileUrl="/resume.pdf"
+                            defaultScale={1}
+                            theme="dark"
+                        />
                     </Worker>
                 </div>
             </div>
