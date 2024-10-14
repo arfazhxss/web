e9e97338a3f6420f66fd414d553b220173801d99
Arfaz Hussain <arfazhussain.zn@gmail.com>
2024-10-13 21:59:39 -0700

    Routine Commit Commit by @arfazhxss on Sun 13 Oct 2024

--- Ve3/src/components/section/pre-hero.tsx
+++ Ve3/src/components/section/pre-hero.tsx
@@ -23,13 +23,56 @@ export default function PreHeroTitle() {
     return (
         <section id="pre-hero">
             <div className="relative flex flex-col items-center justify-center min-h-screen">
+                <div className="absolute 
+                        top-[calc(10%+1px)]         /* Default */
+                        sm:top-[calc(10%-100px)]      /* Small screens */
+                        md:top-[calc(10%-100px)]      /* Medium screens */
+                        lg:top-[calc(10%-100px)]     /* Large screens */
+                        flex flex-row slide-out-to-left-1/2 items-center pr-8 animate-fade-in delay-[10s] z-10">
+                    <div className="blob green"></div>
+                    <p className="font-medium text-sm md:text-md">Available For Work</p>
+                </div>
                 <h1
-                    className={`absolute top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 text-4xl 
-                    sm:text-6xl md:text-6xl lg:text-6xl ${currentTheme === "dark" ? "text-white" : "text-black"
-                        } cursor-default text-edge-outline animate-title font-display whitespace-nowrap opacity-80`}
+                    className={`absolute 
+                        top-[calc(30%+1px)]         /* Default */
+                        sm:top-[calc(20%+1px)]      /* Small screens */
+                        md:top-[calc(20%+5px)]      /* Medium screens */
+                        lg:top-[calc(20%+10px)]     /* Large screens */
+                        text-5xl                    /* Default font size */
+                        sm:text-6xl                 /* Small screens font size */
+                        md:text-7xl                 /* Medium screens font size */
+                        lg:text-8xl                 /* Large screens font size */
+                        left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 
+                        ${currentTheme === "dark" ? "text-white" : "text-black"
+                        } cursor-default text-edge-outline animate-title font-display 
+                        whitespace-nowrap opacity-80`}
                 > Arfaz Hussain
                 </h1>
-                <div className="absolute top-[calc(25%+47px)] animate-fade-in delay-[4s] flex gap-2">
+                <div className="
+                        absolute 
+                        top-[calc(30%+30px)]        /* Default */
+                        sm:top-[calc(20%+35px)]     /* Small screens */
+                        md:top-[calc(20%+47px)]     /* Medium screens */
+                        lg:top-[calc(20%+65px)]     /* Large screens */
+                        animate-fade-in 
+                        delay-[10s] 
+                        flex 
+                        gap-2">
+                    <p className="flex
+                        xs:text-xs 
+                        sm:text-sm 
+                        md:text-lg 
+                        lg:text-2xl
+                        font-medium text-xs text-center text-muted-foreground">
+                        Software Developer based in British Columbia 🇨🇦
+                    </p>
+                </div>
+                <div className="absolute 
+                        top-[calc(30%+70px)] 
+                        sm:top-[calc(20%+70px)]     /* Small screens */
+                        md:top-[calc(20%+90px)]     /* Medium screens */
+                        lg:top-[calc(20%+115px)]    /* Large screens */
+                        animate-fade-in delay-[10s] flex gap-2">
                     <Button asChild variant={'outline'} size="sm">
                         <Link href="/resume.pdf">
                             View Resume <ArrowUpRightIcon className="ml-2 size-4" />
@@ -37,10 +80,15 @@ export default function PreHeroTitle() {
                     </Button>
                     <EmailButton />
                 </div>
-                <div className="absolute top-[calc(39%+100px)] right-1/2 left-1/2 transform -translate-x-1/2 z-10">
+                <div className="absolute 
+                        top-[calc(49%+100px)] 
+                        sm:top-[calc(39%+100px)]     /* Small screens */
+                        md:top-[calc(39%+100px)]    /* Medium screens */
+                        lg:top-[calc(39%+150px)]    /* Large screens */
+                        right-1/2 left-1/2 transform -translate-x-1/2 z-10">
                     <ScrollDownArrow />
                 </div>
             </div>
-        </section>
+        </section >
     );
 }
