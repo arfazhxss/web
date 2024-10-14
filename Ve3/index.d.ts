da03d99aab7ff6bf358c096f43c3796eea381646
Arfaz Hussain <arfazhussain.zn@gmail.com>
2024-10-14 09:13:06 -0700

    Routine Commit Commit by @arfazhxss on Mon 14 Oct 2024

--- Ve3/index.d.ts
+++ Ve3/index.d.ts
@@ -1,2 +1,7 @@
 declare module "pdfjs-dist/build/pdf";
 declare module "pdfjs-dist/build/pdf.worker";
+declare module 'pdfjs-dist/build/pdf.worker.min.js' {
+    const workerPath: string;
+    export default workerPath;
+}
+
--- Ve3/src/app/resume/page.tsx
+++ Ve3/src/app/resume/page.tsx
@@ -1,31 +1,30 @@
+"use client";
+import { Viewer, Worker } from '@react-pdf-viewer/core';
+import '@react-pdf-viewer/core/lib/styles/index.css';
+
 export default function ResumePage() {
     return (
         <div className="bg-background text-foreground max-w-7xl">
             <header className="mb-12 text-center">
-                <h1 className="font-bold mb-2
-                    text-4xl
-                    md:text-4xl 
-                    lg:text-5xl">Arfaz Hossain</h1>
-                <p className="text-muted-foreground mt-[-4px]
-                    text-md 
-                    md:text-md
-                    lg:text-lg
-                    ">Software Engineering Student</p>
+                <h1 className="font-bold mb-2 text-4xl md:text-4xl lg:text-5xl">
+                    Arfaz Hossain
+                </h1>
+                <p className="text-muted-foreground mt-[-4px] text-md md:text-md lg:text-lg">
+                    Software Engineering Student
+                </p>
             </header>
             <div className="mb-12">
-                <h2 className="font-semibold mb-3
-                    text-lg
-                    md:text-lg
-                    lg:text-2xl
-                    ">My Resume</h2>
-                <div className="aspect-[8/23.5] w-full max-w-9xl mx-auto overflow-hidden rounded-lg border bg-muted">
-                    <iframe
-                        src="/resume.pdf"
-                        className="w-full h-full"
-                        title="Arfaz's Resume"
-                    />
+                <h2 className="font-semibold mb-3 text-lg md:text-lg lg:text-2xl">
+                    My Resume
+                </h2>
+                <div className="w-full max-w-9xl mx-auto h-[600px] overflow-hidden rounded-lg border bg-muted">
+                    <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
+                        <Viewer
+                            fileUrl="/resume.pdf"
+                        />
+                    </Worker>
                 </div>
             </div>
         </div>
-    )
+    );
 }
