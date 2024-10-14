730430506181a0e80896dfc0c7ceca566bb6319a
Arfaz Hussain <arfazhussain.zn@gmail.com>
2024-10-14 06:44:12 -0700

    Resume Embed (text corrections) Commit by @arfazhxss on Mon 14 Oct 2024

--- Ve3/src/app/resume/page.tsx
+++ Ve3/src/app/resume/page.tsx
@@ -2,11 +2,22 @@ export default function ResumePage() {
     return (
         <div className="bg-background text-foreground max-w-7xl">
             <header className="mb-12 text-center">
-                <h1 className="text-5xl font-bold mb-2">Arfaz Hossain</h1>
-                <p className="text-xl text-muted-foreground">Software Engineering Student</p>
+                <h1 className="font-bold mb-2
+                    text-4xl
+                    md:text-4xl 
+                    lg:text-5xl">Arfaz Hossain</h1>
+                <p className="text-muted-foreground mt-[-4px]
+                    text-md 
+                    md:text-md
+                    lg:text-lg
+                    ">Software Engineering Student</p>
             </header>
             <div className="mb-12">
-                <h2 className="text-2xl font-semibold mb-3">My Resume</h2>
+                <h2 className="font-semibold mb-3
+                    text-lg
+                    md:text-lg
+                    lg:text-2xl
+                    ">My Resume</h2>
                 <div className="aspect-[8/23.5] w-full max-w-9xl mx-auto overflow-hidden rounded-lg border bg-muted">
                     <iframe
                         src="/resume.pdf"
